cac12d6005606cb8eca76ce03165a20acfd234ac
James Gilliland <jgilliland@apqc.org>
2014-06-13 20:31:56 -0500

    Factor out recipes and support ssl by default

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs and configures redmine as a Rails app in passenger+apache2"
-version           "0.11.1"
+version           "0.12.0"
 
 recipe "redmine", "Installs and configures redmine under passenger + apache2"
 
