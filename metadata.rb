4062250db781b7081c96a7c2fc1b02eb226c5b75
James Gilliland <jgilliland@apqc.org>
2014-06-13 21:12:11 -0500

    Bug fixes and doc updates

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs and configures redmine as a Rails app in passenger+apache2"
-version           "0.12.0"
+version           "0.12.1"
 
 recipe "redmine", "Installs and configures redmine under passenger + apache2"
 
