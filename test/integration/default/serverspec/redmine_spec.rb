0f09b04145c78cde51aa0db94e5562a06b23b6e5
James Gilliland <jgilliland@apqc.org>
2014-08-19 11:31:43 -0500

    Don't tests apache
    This is OS specific and we only care about the ports

--- test/integration/default/serverspec/redmine_spec.rb
+++ test/integration/default/serverspec/redmine_spec.rb
@@ -1,17 +1,8 @@
 require 'spec_helper'
 
-# apache service
-describe service('apache2') do
-  it { should be_enabled }
-  it { should be_running }
-end
-
 describe port(80) do
   it { should be_listening }
 end
-
-describe command("/opt/chef/embedded/bin/bundle exec rake -T redmine:plugins:migrate RAILS_ENV='production'") do
-  let(:path) { '/srv/redmine' }
-#  it { should return_exit_status 0 }
-#  it { should return_stdout 'rake redmine:plugins:migrate  # Migrates installed plugins' }
+describe port(443) do
+  it { should be_listening }
 end
